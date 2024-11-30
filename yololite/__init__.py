2d599ff14cf89a75c3c09f18688fc0007690b951
Dongyun Jinyu <120109253+dongyunjinyu@users.noreply.github.com>
2024-11-30 16:34:17 +0800

    Delete yololite directory

--- yololite/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-if not os.environ.get("OMP_NUM_THREADS"):
-    os.environ["OMP_NUM_THREADS"] = "1"  # default for reduced CPU utilization during training
-
-from yololite.engine.model import Model as YOLO
-
-__all__ = "YOLO"
-
